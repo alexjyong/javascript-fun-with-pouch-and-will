55665f8efcd39dec4262e136de805879183bedab
Alex Yong <alexjyong@gmail.com>
2021-02-05 22:09:44 -0600

    adding in jpg counter.

--- chapter5/doPhotos.js
+++ chapter5/doPhotos.js
@@ -2,9 +2,39 @@ require ('./photos.js')
 // console.log(PHOTOS)
 
 // Filter out just png files
-console.log(PHOTOS.filter(photo => photo.format == 'png'))
+//console.log(PHOTOS.filter(photo => photo.format == 'png'))
 // Now just print their names
-console.log(PHOTOS.filter(photo => photo.format == 'jpg').map(entry => entry.filename))
+//console.log(PHOTOS.filter(photo => photo.format == 'jpg').map(entry => entry.filename))
+
+/*
+ *
+ *
+ *
+ *
+ * let names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice']
+
+let countedNames = names.reduce(function (allNames, name) {
+  if (name in allNames) {
+    allNames[name]++
+  }
+  else {
+    allNames[name] = 1
+  }
+  return allNames
+}, {})
+// countedNames is:
+// { 'Alice': 2, 'Bob': 1, 'Tiff': 1, 'Bruce': 1 }
+*/
+
+let count = PHOTOS.reduce(function (allPhotos, photo) {
+	if (photo.format == "jpg") {
+		allPhotos++;
+	}
+	return allPhotos;	
+}, 0);
+console.log(count);
+//let initialValue = 0;
+//console.log(PHOTOS.reduce( (accumulator, currentValue) => { return currentValue.format =="jpg" } , 0 ));
 
 // Ok here's some things for us to work on together
 // Find any entries with the keyword 'Sunny Crest', print name & filepath
